fd7959b4e8e060df152af042e923b73279899f33
JoelCann <joelcann12@gmail.com>
2023-03-27 22:42:14 +0000

    completed about page

--- src/Routes/about.js
+++ src/Routes/about.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import audi_Q3 from '../assets/Audi_Q3.jpg'
+import audi_logo from '../assets/audi_logo.jpg'
+
 
 function Aboutpage() {
 
     return <div>
         <div className="bg-dark p-3 text-white">
             <h1>About Page</h1>
+        </div>
+        <div className='mt-5'>
+            <img src={audi_logo} style={{ width: 400 }} />
+        </div>
+        <br />
+        <div className='mx-5'>
+            <h4> Who Owns Audi?</h4><bbr />
+            <p>While researching your favorite Audi luxury automobiles near Rock Creek, it's natural to wonder about the history of Audi and the owners of this heralded marque. Have you been asking yourself, "who owns Audi?" or "who makes Audi cars?" The Volkswagen Group makes Audi vehicles, and has a history that runs deep in motorsport heritage. Learn more about who owns Audi, who makes Audi, Audi history, and even how to pronounce Audi, with our team at Audi of North Austin.</p>
+            <br />
+            <h4> Who Makes Audi Vehicles? </h4><bbr />
+            <p>
+                So Killeen drivers, who owns Audi and who makes Audi Cars? Today, that's the Volkswagen Group, as we mentioned previously. However, Audi history has endured many permutations.
+
+                In 1899, August Horch founded his own auto group: Horch & Cie. Due to creative differences with his partner, Herr Horch parted ways to make his own vehicles. His second car company also brought with it a new name: Audi.
+
+                The name "Audi" is derived from the Latin translation of "Horch" or "hark!" which means "listen." Over the next few years, Audi grew famous due to its excellence in motorsports during the early 1900s. In 1964, the Volkswagen Group purchased a 50% stake in Audi, utilizing their manufacturing and engineering expertise. Today, the Volkswagen group owns dozens of high-performance automakers, including Lamborghini, Bugatti, Porsche, and Bentley.
+
+                The question of who owns Audi and who makes Audi is simply answered with: The Volkswagen Auto Group. Although, technically speaking, Audi is public and partially owned by shareholders from across the world, including some Cedar Park drivers.
+
+
+            </p>
+            <br />
+            <h4>Where Are Audi Vehicles Made? </h4><bbr />
+            <p>
+                While they're a German automaker, Audi vehicles are made all over the world. A few countries with Audi manufacturing plants include: Germany, Hungary, Belgium, Mexico, Slovakia, Spain, Russia, Brazil, India, and China. While Audi has plants all over the world, its headquarters remains in Ingolstadt, Germany, and it's culturally significant as a specifically German manufacturer.
+            </p>
         </div>
     </div>
 
--- src/Routes/home.js
+++ src/Routes/home.js
@@ -14,8 +14,8 @@ function Homepage() {
             </div>
 
             <div className='col-md-6 p-5'>
-                <h2 style={{ fontWeight: '700' }}>AUDI AG</h2>
-                <p className='textmarkup'> Audi AG  is a German automotive manufacturer of luxury vehicles headquartered in Ingolstadt, Bavaria, Germany. As a subsidiary of its parent company, the Volkswagen Group, Audi produces vehicles in nine production facilities worldwide.
+                <h2 style={{ fontWeight: '700' }}>AUDI A5</h2>
+                <p className='textmarkup mx-5'> Audi AG  is a German automotive manufacturer of luxury vehicles headquartered in Ingolstadt, Bavaria, Germany. As a subsidiary of its parent company, the Volkswagen Group, Audi produces vehicles in nine production facilities worldwide.
 
                     The origins of the company are complex, going back to the early 20th century and the initial enterprises (Horch and the Audiwerke) founded by engineer August Horch (1868–1951); and two other manufacturers (DKW and Wanderer), leading to the foundation of Auto Union in 1932. The modern Audi era began in the 1960s, when Auto Union was acquired by Volkswagen from Daimler-Benz.[9] After relaunching the Audi brand with the 1965 introduction of the Audi F103 series, Volkswagen merged Auto Union with NSU Motorenwerke in 1969, thus creating the present-day form of the company.</p>
 
